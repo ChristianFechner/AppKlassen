26e8f4bb69c72a127fa049f9c754d53fbab91615
ChristianFechner <kaputtnick01@yahoo.de>
2025-03-18 13:06:56 +0100

    first commit

---
+++ Chatapp.dart
@@ -0,0 +1,13 @@
+class Kontakt {
+  // Atribute
+  String Foto;
+  String Name;
+  String Online;
+
+  // Konstruktor
+  Kontakt(this.Foto, this.Name, this.Online);
+}
+
+void main (){
+  
+}
---
+++ Chatapp2.dart
@@ -0,0 +1,12 @@
+Class Kopfteil{
+  String Inhalt;
+  String Suchbutton;
+  String Einstellung;
+
+ // Konstruktor
+  Kontakt(this.Inhalt, this.Suchbutton this.Einstellung);
+}
+
+void main (){
+  
+}
